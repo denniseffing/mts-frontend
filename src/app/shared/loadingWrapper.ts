397bc10ec3637427cf8b77bc5c18afc5d6efa984
Dennis Effing <dennis.effing@hotmail.com>
2018-09-25 14:57:17 +0200

    Add dedicated rate limit error message.

--- src/app/shared/loadingWrapper.ts
+++ src/app/shared/loadingWrapper.ts
@@ -2,10 +2,11 @@ import { Observable } from 'rxjs/Observable';
 import { catchError, shareReplay } from 'rxjs/operators';
 import { Subject } from 'rxjs/Subject';
 import { of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 export class LoadingWrapper<T> {
-  private readonly _errorLoading$: Subject<boolean> = new Subject<boolean>();
-  readonly errorLoading$: Observable<boolean> = this._errorLoading$.pipe(
+  private readonly _errorLoading$: Subject<HttpErrorResponse> = new Subject<HttpErrorResponse>();
+  readonly errorLoading$: Observable<HttpErrorResponse> = this._errorLoading$.pipe(
     shareReplay(1),
   );
   readonly data$: Observable<T>;
@@ -13,8 +14,8 @@ export class LoadingWrapper<T> {
   constructor(data: Observable<T>) {
     this.data$ = data.pipe(
       shareReplay(1),
-      catchError((_: Error) => {
-        this._errorLoading$.next(true);
+      catchError((error: HttpErrorResponse) => {
+        this._errorLoading$.next(error);
         return of<T>();
       }),
     );
